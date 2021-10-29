7cb5e54fd420448228c54a6756c0a6f35f406336
bimo.ez <bimo.ez@kakaomobility.com>
2021-10-29 18:03:49 +0900

    Initial Commit

---
+++ LBTASwiftuiFirebaseChat/LBTASwiftuiFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTASwiftuiFirebaseChatApp.swift
+//  LBTASwiftuiFirebaseChat
+//
+//  Created by bimo.ez on 2021/10/29.
+//
+
+import SwiftUI
+
+@main
+struct LBTASwiftuiFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
